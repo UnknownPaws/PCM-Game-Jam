8fa37b1af3dd0b3e03e6bf348a41c569cddfb8b4
Edward <edward.odwyer@icloud.com>
2022-03-24 22:08:46 +0000

    Added movement capabilities
    WASD to move, space to jump, mouse to look

---
+++ PCM Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public CharacterController controller;
+    public float speed;
+    public float acceleration;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+    public float jumpHeight;
+
+    bool isGrounded;
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float actualSpeed = speed;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < -2)
+        {
+            velocity.y = -2f;
+            actualSpeed = speed;
+        }
+        else
+        {
+            actualSpeed = speed/1.5f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * actualSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * -acceleration);
+        }
+
+        velocity.y += -acceleration * Time.deltaTime ; // 1/2 a t^2 = x   therefore we need time^2 and half of acceleration downwards
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
